55bd7ff19fbadde0667e0199a5a058e82788fd21
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-07-16 12:39:14 +0530

    Add files via upload

---
+++ RightPascalsTriangle.java
@@ -0,0 +1,17 @@
+public class RightPascalsTriangle {
+    public static void main(String[] args) {
+        int rows = 5;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = rows - 1; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
