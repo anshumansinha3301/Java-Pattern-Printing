814890fd62b32b5d4eb15ecee2d4ca5edf48fa27
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-07-16 12:29:02 +0530

    Add files via upload

---
+++ HeartPattern.java
@@ -0,0 +1,29 @@
+public class HeartPattern {
+    public static void main(String[] args) {
+        int n = 6;
+        for (int i = n / 2; i <= n; i += 2) {
+            for (int j = 1; j < n - i; j += 2) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n; i >= 1; i--) {
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (i * 2) - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
