e134f42809a2aefce5d9f827a46dff592e84c63a
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-07-16 12:29:49 +0530

    Add files via upload

---
+++ HollowSquarePattern.java
@@ -0,0 +1,15 @@
+public class HollowSquarePattern {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
