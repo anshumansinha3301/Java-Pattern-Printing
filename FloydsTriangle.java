edc6e2a496bec4144aec45e53b21bab1c91247a6
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-07-16 12:27:27 +0530

    Add files via upload

---
+++ FloydsTriangle.java
@@ -0,0 +1,12 @@
+public class FloydsTriangle {
+    public static void main(String[] args) {
+        int rows = 5, number = 1;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(number + " ");
+                number++;
+            }
+            System.out.println();
+        }
+    }
+}
