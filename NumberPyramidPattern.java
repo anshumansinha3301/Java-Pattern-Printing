0066d319fd413b62f9e021a3ee487b2dbda4072a
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-07-16 12:26:38 +0530

    Add files via upload

---
+++ NumberPyramidPattern.java
@@ -0,0 +1,14 @@
+public class NumberPyramidPattern {
+    public static void main(String[] args) {
+        int rows = 5;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = rows; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print(k + " ");
+            }
+            System.out.println();
+        }
+    }
+}
